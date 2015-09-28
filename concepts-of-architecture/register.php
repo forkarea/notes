414dda8348bdec63d2158821fe6e3de5c4b4c73a
Adrian Pietka <adrian@pietka.me>
2015-09-28 22:53:24 +0200

    concepts-of-architecture - register

---
+++ concepts-of-architecture/register.php
@@ -0,0 +1,76 @@
+<?php
+
+interface UserFactory {
+    public function createUser(array $data);
+}
+    
+interface UserRepository {
+    public function add(User $user);
+}
+
+class User {
+    private $username;
+    private $password;
+    private $email;
+    
+    public function __construct($username) {
+        $this->username = $username;
+    }
+    
+    public function setPassword($password) {
+        $this->password = $password;
+    }
+    
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+}
+
+class ArrayUserFactory implements UserFactory {
+    public function createUser(array $data) {
+        $user = new User($data['username']);
+       
+        $user->setPassword(md5($data['password'])); // example
+        $user->setEmail($data['email']);
+        
+        return $user;
+    }
+}
+
+class DoctrineUserRepository implements UserRepository {
+    public function add (User $user) {
+        // add user to storage
+        
+        var_dump($user);
+    }
+}
+
+class RegisterUserService {
+    private $users;
+    private $factory;
+    
+    public function __construct(UserRepository $users, UserFactory $factory) {
+        $this->users = $users;
+        $this->factory = $factory;
+    }
+    
+    public function register(array $data) {
+        $this->users->add($this->factory->createUser($data));
+    }
+}
+
+///
+
+$data = [
+    'username' => 'adrianpietka',
+    'password' => 'strong#password',
+    'email' => 'adrian@pietka.me'
+];
+
+$userRepository = new DoctrineUserRepository;
+$userFactory = new ArrayUserFactory;
+$registerService = new RegisterUserService($userRepository, $userFactory);
+
+$registerService->register($data);
+
+// @via zawarstwaabstrakcji.pl
