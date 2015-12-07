95fd497964d7d8f9e713575d8029e5d6424850f1
Adrian Pietka <adrian@pietka.me>
2015-12-07 16:47:00 +0100

    php-7-features

--- php-7-features/scalar-type-declarations.php
+++ php-7-features/scalar-type-declarations.php
@@ -2,8 +2,35 @@
 
 /**
  * PHP 7
- * Two flavours: coercive (default behavior) & strict.
+ * Added support to declarations type: int, string, float, bool.
+ * Two flavours:
+ * - coercive - default behaviour, auto convert type of value,
+ * - strict - throw TypeError when passed value of invalid type.
  */
 
-// setup strict
+// setup strict, declare must be at the top of file
 declare(strict_types=1);
+
+// It's only an example class.
+// $price shouldn't be a float value.
+class Product
+{
+    public function __construct(string $name, int $ammount, float $price, bool $available)
+    {
+        // ...
+    }
+}
+
+try
+{
+    // Ok
+    new Product('Product 1', 1, 1.55, true);
+
+    // Fatal error: Uncaught TypeError: Argument 2 passed to Product::__construct()
+    // must be of the type integer, string given
+    new Product('Product 1', '1', 1.55, true);
+}
+catch (TypeError $e)
+{
+    echo $e->getMessage();
+}
