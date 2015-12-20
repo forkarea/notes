95bc2a09aa6e2c97d0ac60f38d677e0bdb399872
Adrian Pietka <adrian@pietka.me>
2015-12-20 23:20:49 +0100

    kata-angularjs

---
+++ kata-angularjs/http-interceptor-response-error.js
@@ -0,0 +1,19 @@
+// Catch HTTP 401 with Angular.js interceptor
+
+var app = angular.module('LoremIpsumApp', []);
+
+app.config(function ($httpProvider) {
+    $httpProvider.interceptors.push(function ($q, $rootScope) {
+        var catchResponseError = function (rejection) {
+            if (rejection.status === 401) {
+                $rootScope.$broadcast("session.unauthorized");
+            }
+
+            return $q.reject(rejection);
+        };
+
+        return {
+            responseError: catchResponseError
+        }
+    });
+});
